ac0c818e6d61a023d5a5767b8434ee93d8c2a0a7
Dhanasekaran S J <34158742+sjdhanasekaran@users.noreply.github.com>
2018-07-06 15:44:09 +0530

    Create SaveGowtham.java

---
+++ SaveGowtham.java
@@ -0,0 +1,24 @@
+public class SaveGowtham{
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n=sc.nextInt();
+            int[] a=new int[n];
+            for (int i = 0; i <n; i++) {
+                a[i]=sc.nextInt();
+            }
+            long maximum=0L;
+            for (int i = 0; i <n; i++) {
+                for (int k = i + 1; k < n; k++) {
+                    if (a[k] > a[i]) {
+                        maximum += a[k];
+                        break;
+                    }
+                }
+            }
+            System.out.println(maximum);
+            t--;
+        }
+    }
+    }
